93bbf708405ddf635837df55a61a8a1741085768
gharjare <gharjarechetan@gmail.com>
2022-12-01 12:56:45 +0530

    reverse number prblm

---
+++ ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgammimgproblem
+{
+    public class ReverseNumber
+    {
+        public static void CheckReverse()
+        {
+            int num, rem, reverse = 0;
+
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while(num != 0)
+            {
+                rem = num % 10;
+                reverse = reverse * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse number "+ reverse);
+        }
+    }
+}
