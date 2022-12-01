f726a8844e65a9999b9b09fedbb588972494a006
gharjare <gharjarechetan@gmail.com>
2022-12-01 12:52:53 +0530

    perfect number prblm

---
+++ PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgammimgproblem
+{
+    public class PerfectNumber
+    {
+        public static void CheckPerfect()
+        {
+            int sum = 0;
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+
+
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+                
+            if(sum == num)
+            {
+                    Console.WriteLine("{0} Number is pefect",num);
+            }
+            else
+            {
+                    Console.WriteLine("{0} Number is not perfect",num);
+            }
+            
+        }
+    }
+}
