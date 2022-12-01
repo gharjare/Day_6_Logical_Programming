51ea5b2faf3c558fc59373615f7ceb8c8c624e11
gharjare <gharjarechetan@gmail.com>
2022-12-01 12:51:59 +0530

    fibonacci series prblm

---
+++ FibonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgammimgproblem
+{
+    public class FibonacciSeries
+    {
+        public static void CheckSeries()
+        {
+            Console.WriteLine("Check Fibonacci Series");
+
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for(int i = 0; i < 10; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
