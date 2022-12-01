aae80b63434b123b3ef83e2d2f2bbb913ebe1113
gharjare <gharjarechetan@gmail.com>
2022-12-01 13:09:08 +0530

    binary to deci prblm

---
+++ ToBinary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgammimgproblem
+{
+    public class ToBinary
+    {
+        public static void BToDConversion()
+        {
+            float fractionalBinary, binaryNumber, binary, rem, decimalNumber = 0;
+            long factor = 1, integerBinary;
+            Console.WriteLine("\nEnter any number : ");
+            binaryNumber = (float.Parse)(Console.ReadLine()); 
+            integerBinary = (int)(binaryNumber); 
+            fractionalBinary = binaryNumber - integerBinary; 
+            Console.WriteLine($"\nInteger part of given binary number : {integerBinary}");
+            Console.WriteLine($"\nFraction part of given binary number : {fractionalBinary}");
+            binary = binaryNumber;
+            while (integerBinary > 0) 
+            {
+                rem = integerBinary % 10;
+                decimalNumber = decimalNumber + rem * factor;
+                integerBinary = integerBinary / 10;
+                factor = factor * 2;
+            }
+            float p = 0.5F;
+            int temp;
+            while (fractionalBinary > 0) 
+            {
+                fractionalBinary *= 10;
+                temp = (int)fractionalBinary;
+                decimalNumber += temp * p;
+                p *= 0.5F;
+                fractionalBinary -= temp;
+            }
+            Console.WriteLine($"\nDecimal Number of {binary} is : {decimalNumber}");
+        }
+    }
+}
