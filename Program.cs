2c125636e35764546ea3d5eab8ee56997ebb4193
gharjare <gharjarechetan@gmail.com>
2022-12-01 12:46:19 +0530

    program file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using LogicalProgammimgproblem;
+
+namespace LogicalProgrammingProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            // FibonacciSeries.CheckSeries();
+            //PerfectNumber.CheckPerfect();
+            // PrimeNumber.CheckPrime();
+            // ReverseNumber.CheckReverse();
+            //CouponNumber.CheckCoupon();
+            // StopWatch.CheckTime();
+            //TempratureConversion.CheckTemp();
+            //MonthlyPayment.Payment();
+            //DaysOfWeek.Days();
+            // SQRT.FindSqrt();
+            // ToBinary.BToDConversion();
+           // Binary.DToBConversion();
+        }
+    }
+}
